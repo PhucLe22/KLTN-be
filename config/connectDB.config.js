6e2beee246b0a858998fcc75caf6a6c018794c94
Gbao <giabao23479@gmail.com>
2026-03-29 10:03:42 +0700

    update

--- config/connectDB.config.js
+++ config/connectDB.config.js
@@ -1,20 +1,21 @@
-import prismaPkg from '@prisma/client';
-import { PrismaPg } from '@prisma/adapter-pg';
-import pg from 'pg';
-
+import prismaPkg from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import pg from "pg";
 const { PrismaClient } = prismaPkg;
 
 const connectDB = async () => {
   try {
-    const adapter = new PrismaPg(new pg.Pool({
-      connectionString: process.env.DATABASE_URL
-    }));
+    const adapter = new PrismaPg(
+      new pg.Pool({
+        connectionString: process.env.DATABASE_URL,
+      }),
+    );
     const prisma = new PrismaClient({
-      adapter
+      adapter,
     });
     await prisma.$connect();
     console.log("PostgreSQL connected successfully");
-    
+
     return prisma;
   } catch (error) {
     console.error("Unable to connect to PostgreSQL:", error.message);
